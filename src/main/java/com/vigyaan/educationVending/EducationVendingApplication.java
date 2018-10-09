1b724130d1deb860fc2623feee101e6c7e4ae687
Mohith Khatri <mohithkhatri100@gmail.com>
2018-10-09 17:57:17 +0530

    Initial commit

---
+++ src/main/java/com/vigyaan/educationVending/EducationVendingApplication.java
@@ -0,0 +1,12 @@
+package com.vigyaan.educationVending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationVendingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationVendingApplication.class, args);
+	}
+}
